df7f2623d65fe354fa9aa70c533203d213f572d5
RafaelAlexander <Rafael-Olmos@hotmail.com.ar>
2019-05-02 00:24:20 -0300

    Cree particiones y temporales. Con sus respectivos bloques

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/ManejoArchivo.c 
+../src/ManejoDeArchivosBin.c 
 
 OBJS += \
-./src/ManejoArchivo.o 
+./src/ManejoDeArchivosBin.o 
 
 C_DEPS += \
-./src/ManejoArchivo.d 
+./src/ManejoDeArchivosBin.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
--- src/ManejoDeArchivosBin.c
+++ src/ManejoDeArchivosBin.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
- Name        : ManejoArchivo.c
- Author      : 
+ Name        : ManejoDeArchivosBin.c
+ Author      : Rafael
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -16,12 +16,20 @@
 
 void crearBitMapFS(char path[]);
 void crearTables(char path[]);
-void crearBloquesDatos(char path[]);
+void crearBloquesDatos(char path[],u_int particiones);
 void crearUnaTabla(char path[],char nombreDeLaTabla[],uint8_t tipoDeConsistencia,u_int tiempoDeCompactacion,u_int particiones);
 void crearMetaDataTabla(char path[],uint8_t tipoDeConsistencia,u_int tiempoDeCompactacion,u_int particiones);
 void completarDatosDeMetaData(char path[],uint8_t tipoDeConsistencia,u_int tiempoDeCompactacion,u_int particiones);
-void lecturaDatosDeMetaData(char path[]);
+char* lecturaDatosDeMetaData(char path[],char[]);
+
 enum  TDeCons{SH=0,SHC=1,EC=2};
+
+typedef struct{
+	double timeStamp;
+	uint16_t key;
+	char* value;
+}Registro;
+
 int main(void) {
 	char path[100];
 		//char *path_aux = config_get_string_value(config,"PUNTOMONTAJE");
@@ -30,15 +38,17 @@ int main(void) {
 		uint8_t codDeConsistencia = SH;
 		u_int tiempoDeCompactacion=60000;
 		u_int particiones=4;
+		u_int16_t key=3;
 		char* nombreDeLaTabla="TablaA";
 		if(mkdir(path, 0777) != 0){
 			printf("No se pudo crear Carpeta\n");
 		}
+
 		crearBitMapFS(path);
 		crearTables(path);
 		crearUnaTabla("/home/utnso/Laureano/LISSANDRA_FS/Table/",nombreDeLaTabla,codDeConsistencia,tiempoDeCompactacion,particiones);//se podria settear el config.log con un campo DIRTABLAS
-		lecturaDatosDeMetaData("/home/utnso/Laureano/LISSANDRA_FS/Table/TablaA/");
-		crearBloquesDatos(path);
+		crearBloquesDatos(path,particiones);
+		printf(lecturaDatosDeMetaData("/home/utnso/Laureano/LISSANDRA_FS/Table/",nombreDeLaTabla));
 	return EXIT_SUCCESS;
 }
 
@@ -55,20 +65,11 @@ void crearBitMapFS(char path[]){
 	//<commons/bitarray.h>
 }
 void crearTables(char path[]){
-	char path_aux[100];
+	char* path_aux[100];
 	strcpy(path_aux,path);
 	strcat(path_aux, "Table/");
 	if(mkdir(path_aux, 0777) != 0){
 		printf("No se pudo crear la carpeta /Tables.\n");
-	}
-}
-
-void crearBloquesDatos(char path[]){
-	char path_aux[100];
-	strcpy(path_aux,path);
-	strcat(path_aux, "Bloques/");
-	if(mkdir(path_aux, 0777) != 0){
-		printf("No se pudo crear /Bloques\n");
 	}
 }
 
@@ -89,6 +90,7 @@ void crearMetaDataTabla(char path[],uint8_t tipoDeConsistencia,u_int tiempoDeCom
 	char path_aux[100];
 	strcpy(path_aux,path);
 	strcat(path_aux,"Metadata.bin");
+	crearParticionesYTemporales(path,particiones);
 	//completarDatosDeMetaData(path_aux,data);
 	completarDatosDeMetaData(path_aux,tipoDeConsistencia,tiempoDeCompactacion,particiones);
 }
@@ -96,14 +98,10 @@ void completarDatosDeMetaData(char path[],uint8_t tipoDeConsistencia,u_int tiemp
 	char buffer[100];
 	int nbytes;
 	//buffer="CONSISTENCY=SC\nPARTITIONS=4\nCOMPACTION_TIME=60000";
-	printf("\n%d\n",tipoDeConsistencia);
-	printf("\n%d\n",tiempoDeCompactacion);
-	printf("\n%d\n",particiones);
 	//sprintf(buffer,"PARTITIONS=%d\nCOMPACTION_TIME=%d\n",particiones,tiempoDeCompactacion);
 	switch(tipoDeConsistencia){
 	case SH:
 		//Hice este sprintf para probar que no habia error
-		printf("Entre");
 		sprintf(buffer,"CONSISTENCY=SC\nPARTITIONS=%d\nCOMPACTION_TIME=%d",particiones,tiempoDeCompactacion);
 		break;
 	case SHC:
@@ -125,15 +123,23 @@ void completarDatosDeMetaData(char path[],uint8_t tipoDeConsistencia,u_int tiemp
 	}
 	fclose(f);
 }
-void lecturaDatosDeMetaData(char path[]){
-		char dirMetaData[100];
-		//char** guardado;
-		strcpy(dirMetaData,path);
-		strcat(dirMetaData,"Metadata.bin");
+char* lecturaDatosDeMetaData(char path[],char nombreDeLaTabla[]){
+		FILE* f;
+		char path_aux[100];
+		char aux[50];
+		char valoresQueNecesito[50]="";
+		strcpy(aux,nombreDeLaTabla);
+		strcpy(path_aux,path);
+		strcat(aux,"/");
+		strcat(path_aux,aux);
+		if(opendir(path_aux)==NULL){
+					printf("No existe tabla correspondiente\n");
+		}
+		strcat(path_aux,"Metadata.bin");
 		int nbytes;
 	    char *contenido;
 	    char *delimitador="=\n";
-	    FILE* f=fopen(dirMetaData,"rb");
+	    f=fopen(path_aux,"rb");
 
 	    if(f){
 
@@ -141,17 +147,83 @@ void lecturaDatosDeMetaData(char path[]){
 	    contenido = (char *)malloc(nbytes+1);
 	    fread (contenido, nbytes, 1, f);
 	    contenido[nbytes] = 0;
-	    printf(contenido);
 	   // char* token=strtok_r(contenido,delimitador,guardado);
 	    char* token=strtok(contenido,delimitador);
 	    if(token!=NULL){
 	    	while(token!=NULL){
-	    	if(strstr(token,"CONSISTENCY")==NULL&&strstr(token,"PARTITIONS")==NULL&&strstr(token,"COMPACTION_TIME")==NULL)
-	    			printf("\n%s\n",token);
+	    	if(strstr(token,"CONSISTENCY")==NULL&&strstr(token,"PARTITIONS")==NULL&&strstr(token,"COMPACTION_TIME")==NULL){
+	    		sprintf(aux,"%s\n",token);
+	    		printf(aux);
+	    		strcat(valoresQueNecesito,aux);
+	    	}
 	    		//token=strtok_r(NULL,delimitador,guardado);
 	    		token=strtok(NULL,delimitador);
 	    	}
 	    }
 	    }
+	    else{
+	    	printf("Error en lectura de archivo");
+	    	fclose(f);
+	    }
 	    fclose(f);
+	    return valoresQueNecesito;
+}
+void crearParticionesYTemporales(char path[],u_int particiones){
+	char aux[100];
+	char temporal[100];
+	char particion[100];
+	int contador=1;
+	FILE* f;
+	while(contador<=particiones){
+	//DirectorioDeParticion
+	strcpy(particion,path);
+	sprintf(aux,"%d.bin",contador);
+	strcat(particion,aux);
+	//DirectorioDeTemporal
+	strcpy(temporal,path);
+	sprintf(aux,"%d.tmp",contador);
+	strcat(temporal,aux);
+	//Sube Contador
+	contador++;
+	//CreacionDeParticion
+	f = fopen(particion, "wb+");
+	fclose(f);
+	//CreacionDeTemporal
+	f=fopen(temporal,"wb+");
+	fclose(f);
+	}
+}
+
+void crearBloquesDatos(char path[],u_int particiones){
+	char path_aux[100];
+	strcpy(path_aux,path);
+	strcat(path_aux, "Bloques/");
+	if(mkdir(path_aux, 0777) != 0){
+		printf("No se pudo crear /Bloques\n");
+	}
+	crearBloquesDeDatosDeCadaParticion(path_aux,particiones);
+}
+
+void crearBloquesDeDatosDeCadaParticion(char path[],u_int particiones){
+	char path_aux[100];
+	char bloquesDeDatos[100];
+	char* contenido="SIZE=0\nBLOCKS=[]";
+	FILE* f;
+	int nbytes;
+	int contador=1;
+	while(contador<=particiones){
+		strcpy(path_aux,path);
+		sprintf(bloquesDeDatos,"%d.bin",contador);
+		strcat(path_aux,bloquesDeDatos);
+		nbytes=strlen(contenido);
+		f = fopen(path_aux, "wb+");
+		if(f){
+		//Mando tamaño de string
+		fwrite(&nbytes,sizeof nbytes,1,f);
+		//Mando String
+		fwrite(contenido,nbytes,1,f);
+		}
+		fclose(f);
+		contador++;
+	}
 }
